95c757f0de659c4fb3df93abd38067fcb526ded2
James L McCreight <mccreigh@gmail.com>
2017-11-06 07:10:00 -0700

    Some basic functions of testing

--- testing/test_local.sh
+++ testing/test_local.sh
@@ -3,28 +3,21 @@
 # Purpose:
 #          
 # Arguments:
-#
+#  1) commit?
+#  2) docker image
 # Note: 
 #
 # Usage:
 # 
 
-# Default image is wrf_hydro_dev
-#image=${3-wrfhydro/testing}
+# Default image is wrf_hydro_testing
+image=${1-wrfhydro/testing}
 
-# JLM: how do we know where this script is?
-whDockerPath=`grep "wrf_hydro_docker=" ~/.wrf_hydro_tools | cut -d '=' -f2 | tr -d ' '` 
-if [[ -z $whDockerPath ]]; then
-    echo "Warning wrf_hydro_docker path is not specified in ~/.wrf_hydro_tools"
-else
-    if [[ ! -d $whDockerPath ]]; then
-	echo "Warning: wrf_hydro_docker path ($whDockerPath) does not exist."
-    fi
-fi
-source $whDockerPath/dev/prelude_to_docker_run.sh || exit 1
 
-# pass the environment and the working dir to 
-docker run -it ${envToPass} ${dirsToMnt} $image run `pwd` $1 $2
+docker run -it \
+       -e GITHUB_AUTHTOKEN=$GITHUB_AUTHTOKEN \
+       -e GITHUB_USERNAME=$GITHUB_USERNAME \
+       $image local
 
 
 exit 0
