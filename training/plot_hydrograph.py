c008005bebddf4521e61a5e7a9296f7579436060
jmills-ncar <jmills@ucar.edu>
2018-03-19 19:25:40 -0600

    travis updates

--- training/plot_hydrograph.py
+++ training/plot_hydrograph.py
@@ -58,7 +58,7 @@ def plot_hydrographs(channel_files: list,image_filename: str,feature_id: int,obs
         hydrograph = ggplot(chrtout_data,aes(x='time',y='value',color='variable')) + \
             geom_line() + \
             scale_x_datetime(breaks='1 days') + \
-            labs(x='Date',y='Streamflow, cms',title='Modelled and Observed streamflow at feature_id '+feature_id) + \
+            labs(x='Date',y='Streamflow, cms',title='Modelled and Observed streamflow at feature_id '+str(feature_id)) + \
             theme_bw()
         hydrograph.save(image_filename,'png',height=8,width=8)
     else:
@@ -66,7 +66,7 @@ def plot_hydrographs(channel_files: list,image_filename: str,feature_id: int,obs
         hydrograph = ggplot(chrtout_data,aes(x='time',y='streamflow')) + \
             geom_line() + \
             scale_x_datetime(breaks='1 days') + \
-            labs(x='Date',y='Streamflow, cms',title='Modelled and Observed streamflow at feature_id '+feature_id) + \
+            labs(x='Date',y='Streamflow, cms',title='Modelled and Observed streamflow at feature_id '+str(feature_id)) + \
             theme_bw()
         hydrograph.save(image_filename,'png',height=8,width=8)
 
