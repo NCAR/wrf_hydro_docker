4237b7e037c71f4bc4bc2ba36086311f5e789690
James McCreight <jamesmcc@ucar.edu>
2018-05-01 14:21:33 -0600

    with feeling:separated build and deploy scripts (still untested). removed dev/rwrfhydro

---
+++ build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+depends="$(cat build_deploy_deps.txt)"
+
+for dd in $depends; do
+
+    echo '------------------------------'
+    echo "Building:  $dd"
+
+    dd_dirname=$(dirname $dd)
+    cd $dd_dirname
+
+    ./build.sh || exit 1
+
+    cd - 2&>1 > /dev/null
+    echo
+    
+done
+
+exit 0
+
