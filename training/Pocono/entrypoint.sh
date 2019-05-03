8f369d2e388241490560e7b4c5d808ab53c54350
Ryan Cabell <rcabell@ucar.edu>
2019-05-03 15:05:29 -0600

    Additional Pocono/rfc-training updates
    Change `wrf-hydro` to `nwm`
    New example case tarball
    Add C++ suppose to dev:base MPICH build to support ESMF

--- training/Pocono/entrypoint.sh
+++ training/Pocono/entrypoint.sh
@@ -8,7 +8,7 @@ echo "Open your browser to the following address to access notebooks"
 echo -e "\033[92;7mhttp://localhost:8888\033[0m"
 echo
 echo -e "The password to login is:"
-echo -e "\033[92;7mwrfhydrotraining\033[0m"
+echo -e "\033[92;7mnwm-training\033[0m"
 echo 
 echo "Press ctrl-C then type 'y' then press return to shut down container." 
 echo "NOTE ALL WORK WILL BE LOST UNLESS copied out of the container"
