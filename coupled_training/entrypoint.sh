a2a0c35742d1efed7dbff181e30b20cfaff35084
Katelyn FitzGerald <katelynw@ucar.edu>
2019-10-16 08:36:21 -0600

    update coupled Dockerfile

--- coupled_training/entrypoint.sh
+++ coupled_training/entrypoint.sh
@@ -41,7 +41,7 @@ echo -e "\e[0;49;32m-----------------------------------\e[0m"
 echo -e "Training Jupyter notebook server running"
 echo
 echo "Open your browser to the following address to access notebooks"
-echo -e "\033[33;5;7mlocalhost:8887\033[0m"
+echo -e "\033[33;5;7mhttp://localhost:8887\033[0m"
 echo
 echo -e "The password to login is:"
 echo -e "\033[33;5;7mwrfhydrotraining\033[0m"
