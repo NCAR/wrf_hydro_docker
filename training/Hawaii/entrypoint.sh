ae94ccc4c65e517cf5ae8e3730f9a7415b783505
Katelyn FitzGerald <katelynw@ucar.edu>
2020-09-01 21:35:55 -0600

    Add Hawaii training Dockerfile and additional scripts

---
+++ training/Hawaii/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo
+echo -e "\e[0;49;32m-----------------------------------\e[0m"
+echo -e "Training Jupyter notebook server running"
+echo
+echo "Open your browser to the following address to access notebooks"
+echo -e "\033[92;7mhttp://localhost:8888\033[0m"
+echo
+echo -e "The password to login is:"
+echo -e "\033[92;7mwrfhydrotraining\033[0m"
+echo 
+echo "Press ctrl-C then type 'y' then press return to shut down container." 
+echo "NOTE ALL WORK WILL BE LOST UNLESS copied out of the container"
+
+jupyter notebook --ip 0.0.0.0 --no-browser &> /dev/null
