62fae511c05187feb9cb69b0ea00c08cbe6208da
Katelyn FitzGerald <katelynw@ucar.edu>
2020-10-05 20:41:27 -0600

    edit Hawaii training Dockerfile and entrypoint to pull code and lessons at run time

--- training/Hawaii/entrypoint.sh
+++ training/Hawaii/entrypoint.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+###Change the versions here
+code_branch=v5.2.x
+training_branch=master
+###########################
+
+
+###########################
+###No need to edit below here
+echo -e "\e[4;49;34m WRF-Hydro Training Container\e[0m"
+
+echo
+echo -e "\e[0;49;32m-----------------------------------\e[0m"
+echo -e "\e[7;49;32mRetrieving the model code\e[0m"
+
+git clone --branch ${code_branch} https://github.com/NCAR/wrf_hydro_nwm_public
+mv /home/docker/wrf_hydro_nwm_public /home/docker/wrf-hydro-training/wrf_hydro_nwm_public
+
+echo "Retrieved the following code: $version"
+
+echo
+echo -e "\e[0;49;32m-----------------------------------\e[0m"
+echo -e "\e[7;49;32mRetrieving WRF-Hydro training\e[0m"
+
+git clone --branch ${training_branch} https://github.com/NCAR/wrf_hydro_training
+mv /home/docker/wrf_hydro_training/lessons/training /home/docker/wrf-hydro-training/lessons
+rm -rf /home/docker/wrf_hydro_training/
+
+echo "Retrieved the following training: ${training_branch}"
+
 echo
 echo -e "\e[0;49;32m-----------------------------------\e[0m"
 echo -e "Training Jupyter notebook server running"
